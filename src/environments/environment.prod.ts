ad19677fa0c025f9bd127eeeb4efdedb91f07d8e
Lucas Pereira Lopes <lkogici@gmail.com>
2020-01-06 23:52:54 +0000

    New: Corrigida variável de ambiente de produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyAq1ZXJfWv4HtK5jCMBMFP3ntbch5M13c0",
+    authDomain: "workcontrolplatform.firebaseapp.com",
+    databaseURL: "https://workcontrolplatform.firebaseio.com",
+    projectId: "workcontrolplatform",
+    storageBucket: "workcontrolplatform.appspot.com",
+    messagingSenderId: "1057000872172",
+    appId: "1:1057000872172:web:4d700c012475a3b3d1adc9",
+    measurementId: "G-3VYPDD8MH0"
+  }
 };
